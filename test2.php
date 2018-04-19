d0fc1d724708d5484ae5ab46c40da8197db3ffc2
李硕 <598684053@qq.com>
2018-04-19 20:28:57 +0800

    asdadasdadadsad

--- test2.php
+++ test2.php
@@ -15,13 +15,20 @@ echo 1;
 echo 2;
 echo 1;
 echo 1;
+<<<<<<< HEAD
 echo 2;echo 2;echo 2;echo 2;
 echo 2;echo 2;echo 2;echo 2;
 echo 2;echo 2;echo 2;echo 2;
+=======
 
 echo 1;
 
+<<<<<<< HEAD
 
 
 
 echo "asdadadada"
+=======
+echo 1;
+>>>>>>> dmo-test2
+>>>>>>> parent of af1a334... dmo-test3
